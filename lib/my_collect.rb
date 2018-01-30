04ec5c45d8086f3ece9bfda993052e2ac59ba41f
Harleigh Abel <harleighabel@gmail.com>
2018-01-30 02:15:29 +0000

    automatically backed up by learn

--- lib/my_collect.rb
+++ lib/my_collect.rb
@@ -1,4 +1,7 @@
 languages =['ruby', 'javascript', 'python', 'objective-c']
 students = ['Tim Jones', 'Tom Smith', 'Sophie Johnson', 'Antoin Miller']
 
-
+def my_collect(array)
+  while !(array.empty?)
+  end
+end  
