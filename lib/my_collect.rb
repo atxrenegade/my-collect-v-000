028621800b9129f0a2db90624ed6b589bc62ad42
Harleigh Abel <harleighabel@gmail.com>
2018-01-30 02:17:43 +0000

    automatically backed up by learn

--- lib/my_collect.rb
+++ lib/my_collect.rb
@@ -3,6 +3,9 @@ students = ['Tim Jones', 'Tom Smith', 'Sophie Johnson', 'Antoin Miller']
 
 def my_collect(array)
   while !(array.empty?)
+    n = 0
+    while n < array.length
+      yield array(n)
     break
   end
-end  
+end
